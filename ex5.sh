5990541de976c2102a00e64754688a9924cd2980
Romain Deleuze <39342135+TheWolfInstinct@users.noreply.github.com>
2021-02-24 09:46:34 +0100

    Update ex5.sh

--- ex5.sh
+++ ex5.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]
+then
+  echo "Aucun fichier en argument"
+  exit 1
+fi
+
 FILE=$1
 
-if [ -f "$FILE" ]; then
-  echo "File exists and it's a file"
-elif [ -d "$FILE" ]; then
-  echo "File exists and it's a directory"
-elif [ -x "$FILE" ]; then
-  echo "Executable"
+if [ -e "$FILE" ]; then
+  echo "File Exists"
+  if [ -f "$FILE" ]; then
+    echo "File exists and it's a file"
+    elif [ -x "$FILE" ]; then
+      echo "Executable"
+  elif [ -d "$FILE" ]; then
+    echo "File exists and it's a directory"
 else
   echo "File doesn't exist"
 fi
