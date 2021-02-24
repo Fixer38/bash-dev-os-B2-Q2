ce70d0bc6d25ea54ecbdc09b76731753459c7ba6
Romain Deleuze <39342135+TheWolfInstinct@users.noreply.github.com>
2021-02-24 09:19:04 +0100

    Update FIXED ex4.sh

--- ex4.sh
+++ ex4.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-
-while getopts ":al:" opt; do
-  case ${opt} in
-    a)
-      for file in /var/log/*.log /var/log/**/*.log
-      do
-        cat $file
-      done
-      ;;
-    l)
-      file=$OPTARG
-      cat "/var/log/$file"
-      ;;
-    *)
-      echo "Unknown Option"
-      ;;
-  esac
-done
-
-for file in /var/log/*.log /var/log/**/*.log
-  do
-    echo $file
+log=/var/log
+if [ $# = 0 ]
+then
+  ls -R $log
+else
+  while getopts ":al:" opt; do
+    case ${opt} in
+      a)
+        for file in $:ls -R $log
+        do
+          cat $log/$file
+        done
+        ;;
+      l)
+        file=$OPTARG
+        if [ -e /var/log/$file ]
+        then
+          cat /var/log/$file
+        else
+          echo "Fichier de log inconnu"
+        fi
+        ;;
+      *)
+        echo "Unknown Option"
+        ;;
+    esac
   done
+fi
